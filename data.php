1291db7a4735a4a0e358337240b09cd0f95b5491
imerla1 <giorgi1048@yahoo.com>
2020-07-21 07:36:17 +0400

    added success html

--- data.php
+++ data.php
@@ -1,4 +1,9 @@
 <?php
+// Turn off all error reporting
+    error_reporting(0);
+?>
+
+<?php
 include_once './db.php';
 $id = $_POST['child_id_number'];
 $sql = "SELECT * from user_entries where child_id_number=$id";
--- update.php
+++ update.php
@@ -6,5 +6,5 @@
     $id = $_POST['idnumber'];
     $sql = "UPDATE user_entries SET number='$phone' WHERE child_id_number='$id'";
     $res = mysqli_query($conn, $sql);
-    
+    include "success.html"
 ?> 
