5a88646e93f9738ac4d2b110c2b1370377c399a4
imerla1 <giorgi1048@yahoo.com>
2020-07-21 18:13:37 +0400

    date doesnt an on db

--- data.php
+++ data.php
@@ -8,21 +8,23 @@ include_once './db.php';
 $id = $_POST['child_id_number'];
 $sql = "SELECT * from user_entries where child_id_number=$id";
 $result = mysqli_query($conn, $sql);
-$name = false;
-$mom = false;
-$dad = false;
+// $name = false;
+$mom_id = false;
+$dad_id = false;
 $phone_number = false;
+$birth_date = false;
 
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
-        $name = $row['child_name'];
-        $mom = $row['first_parent_name'];
-        $dad = $row['second_parent_name'];
-
+        // $name = $row['child_name'];
+        $mom_id = $row['first_parent_id_number'];
+        $dad_id = $row['second_parent_id_number'];
+        $birth_date = $row['birth'];
         if ($row['number']) {
             $phone_number = $row['number'];
         }
+        
     }
 }
 
@@ -34,38 +36,60 @@ if ($result->num_rows > 0) {
 
 <?php if ($result->num_rows > 0) { ?>
     <!DOCTYPE html>
-    <html lang="en">
+    <html>
 
     <head>
-        <meta charset="UTF-8">
+        
+        <meta http-equiv=Content-Type content="text/html; charset=windows-1252">
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>2nd auth</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <style>
+            .ss {
+                margin: 30px;
+            }
+        </style>
     </head>
 
     <body>
         <div class="jumbotron">
-            <h1 class="display-4">პერსონალური ინფორამცია</h1>
-
+            <h1 class="display-5">პერსონალური ინფორამცია</h1>
+            
             <ul class="list-group" style="width: 50%;">
 
-                <li class="list-group-item"><strong>ბავშვის სახელი და გვარი: </strong><?php echo $name ?></li>
-                <li class="list-group-item"><strong>დედის სახელი და გვარი: </strong><?php echo $mom ?></li>
-                <li class="list-group-item"><strong>მამის სახელი და გვარი: </strong><?php echo $dad ?></li>
+                <li class="list-group-item"><strong>ბავშვის პირადი ნომერი: </strong><span><?php echo $id ?></span></li>
+                <li class="list-group-item"><strong>დედის პირადი ნომერი: </strong><span><?php echo $mom_id ?></span></li>
+                <li class="list-group-item"><strong>მამის პირადი ნომერი: </strong><span><?php echo $dad_id ?></span></li>
+                <li class="list-group-item"><strong>დაბადების თარიღი: </strong><span><?php echo $birth_date ?></span> 
+            
+                <span><input value="თარირის შეცვლა" type="submit" id='submit-btn-date' class="btn btn-success ss mt-4 lg mb-4px"></span>
+                <input type="hidden" id='datetime' name='datetime' value="ახალი თარიღი">
+            </li>
+
 
             </ul>
             <hr class="my-4">
             <form action="update.php" method="POST">
                 <div class="form-group">
-                    <label for="child_phone">შეიყვანეთ ტელეფონის ნომერი</label>
-                    <input style="width:15%;" pattern="[0-9]{3}[0-9]{3}[0-9]{3}" title="არასწორი ფორმატი" type="text" class="form-control" id="child_phone" name="child_phone" required="" placeholder="5XX XXX XXX">
+                    <label for="child_phone">შეიყვანეთ ტელეფონის ნომერი <strong>(აუცილებელია)</strong></label>
+                    <input style="width:35%; border:1px solid red;" pattern="[0-9]{3}[0-9]{3}[0-9]{3}" title="არასწორი ფორმატი" type="text" class="form-control" id="child_phone" name="child_phone" required="" placeholder="5XX XXX XXX">
                     <input type="hidden" name="idnumber" id="idnumber" value=<?php echo $id ?>>
-                    <input value="დასტური" type="submit" id='submit-btn' class="btn btn-primary mt-4 lg">
+                    <input value="დასტური" type="submit" id='submit-btn' class="btn btn-primary ss lg">
                     
                 </div>
             </form>
 
         </div>
+        <script>
+            let btn = document.getElementById('submit-btn-date')
+            btn.addEventListener("click", 
+                function e(){
+                    document.getElementById('datetime').setAttribute('type', 'date')
+                    
+                }
+            )
+        </script>
     </body>
 
     </html>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Turn off all error reporting
-    error_reporting(0);
+error_reporting(0);
 ?>
 
 <?php
@@ -9,6 +9,7 @@ include_once './db.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,19 +17,22 @@ include_once './db.php';
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
 </head>
+
 <body>
     <div class="jumbotron">
-        <h1 class="display-4">მოგესალმებით!</h1>
+        <h1 class="display-5">მოგესალმებით!</h1>
         <p class="lead">შეიყვანეთ ბავშვი პირადობის დამადასტურებელი ნომერი</p>
         <hr class="my-4">
         <form action="data.php" method="POST">
             <div class="form-group">
                 <label for="child_id_number">პირადი ნომერი:</label>
-                <input style="width:40%;" type="text" class="form-control" id="child_id_number" name="child_id_number" required="">
-                <input value="დასტური" type="submit" id='submit-btn' class="btn btn-primary mt-4 lg" >
+                <input style="width:45%" type="text" class="form-control" id="child_id_number" name="child_id_number" required="">
+                <input value="დასტური" type="submit" id='submit-btn' class="btn btn-primary mt-4 lg">
             </div>
         </form>
-      </div>
-      
+    </div>
+    
+
 </body>
+
 </html>
